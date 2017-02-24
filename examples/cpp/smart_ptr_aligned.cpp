1085676bfd6c207f1ce9d870fd0ba939a6f25a0d
Phillip Johnston <phillip.johnston@gatech.edu>
2017-02-24 09:51:59 -0800

    Give shared pointers the same templated treatment

--- examples/cpp/smart_ptr_aligned.cpp
+++ examples/cpp/smart_ptr_aligned.cpp
@@ -18,9 +18,15 @@ static void aligned_free_wrapper(void * ptr)
 template<class T> using unique_ptr_aligned = std::unique_ptr<T, decltype(&aligned_free)>;
 
 template<class T>
-unique_ptr_aligned<T> aligned_ptr(size_t align, size_t size)
+unique_ptr_aligned<T> aligned_uptr(size_t align, size_t size)
 {
 	return unique_ptr_aligned<T>(static_cast<T*>(aligned_malloc(align, size)), &aligned_free_wrapper);
+}
+
+template<class T>
+std::shared_ptr<T> aligned_sptr(size_t align, size_t size)
+{
+	return std::shared_ptr<T>(static_cast<T*>(aligned_malloc(align, size)), &aligned_free_wrapper);
 }
 
 int main(void)
@@ -31,17 +37,19 @@ int main(void)
 	* Note that rather than calling new(uint8_t), we call aligned_malloc to get our aligned memory
 	*/
 	unique_ptr_aligned<uint8_t[]> x(static_cast<uint8_t*>(aligned_malloc(8, 1024)), &aligned_free_wrapper);
-	auto y = aligned_ptr<uint8_t>(32, 100);
+	auto y = aligned_uptr<uint8_t>(32, 100);
 
 	/**
 	* Why don't we need a shared pointer to also have an alias?
 	* The deleter is not part of the type, simply the constructor
 	*/
 	std::shared_ptr<uint8_t> z(static_cast<uint8_t*>(aligned_malloc(32, 128)), &aligned_free_wrapper);
+	auto a = aligned_sptr<uint8_t>(64, 100);
 
 	printf("x (unique) has alignment 8: %p\n", x.get());
 	printf("y (unique) has alignment 32: %p\n", y.get());
 	printf("z (shared) has alignment 32: %p\n", z.get());
+	printf("a (shared) has alignment 64: %p\n", a.get());
 
 	/**
 	* This will cause the deleter to be called (aligned_free)
@@ -54,7 +62,7 @@ int main(void)
 	* Once we leave main() the pointers will go out of scope and the deleter we specified
 	* will be called automatically.
 	*/
-	printf("Leaving main. X and Z will be automatically destructed\n");
+	printf("Leaving main. Remaining pointers will be automatically destructed\n");
 
 	return 0;
 }
