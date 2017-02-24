04834523e41a0e74418e32f60ad9af7d31f2e89a
Phillip Johnston <phillip.johnston@gatech.edu>
2017-02-24 10:06:55 -0800

    Comment new code in aligned pointer example

--- examples/cpp/smart_ptr_aligned.cpp
+++ examples/cpp/smart_ptr_aligned.cpp
@@ -17,12 +17,27 @@ static void aligned_free_wrapper(void * ptr)
 */
 template<class T> using unique_ptr_aligned = std::unique_ptr<T, decltype(&aligned_free)>;
 
+/**
+* We can create a template function that simplifies our declarations of aligned
+* unique pointers. Alignment and size are passed through to aligned malloc, and
+* aligned free is always used as the deleter.  We then generate the correct pointer
+* type based on the templated call
+*/
 template<class T>
 unique_ptr_aligned<T> aligned_uptr(size_t align, size_t size)
 {
 	return unique_ptr_aligned<T>(static_cast<T*>(aligned_malloc(align, size)), &aligned_free_wrapper);
 }
 
+/**
+* We can create a template function that simplifies our declarations of aligned
+* shared pointers. Alignment and size are passed through to aligned malloc, and
+* aligned free is always used as the deleter.  We then generate the correct pointer
+* type based on the templated call
+*
+* Notice here that the shared pointer doesn't need a special type due to the deleter
+* The deleter type is only required for the unique pointer.
+*/
 template<class T>
 std::shared_ptr<T> aligned_sptr(size_t align, size_t size)
 {
@@ -37,6 +52,11 @@ int main(void)
 	* Note that rather than calling new(uint8_t), we call aligned_malloc to get our aligned memory
 	*/
 	unique_ptr_aligned<uint8_t[]> x(static_cast<uint8_t*>(aligned_malloc(8, 1024)), &aligned_free_wrapper);
+
+	/*
+	* Instead of the lengthy declaration above, we can use our templated
+	* aligned_uptr function for a much simpler declaration
+	*/
 	auto y = aligned_uptr<uint8_t>(32, 100);
 
 	/**
@@ -44,6 +64,11 @@ int main(void)
 	* The deleter is not part of the type, simply the constructor
 	*/
 	std::shared_ptr<uint8_t> z(static_cast<uint8_t*>(aligned_malloc(32, 128)), &aligned_free_wrapper);
+
+	/*
+	* Instead of the lengthy declaration above, we can use our templated
+	* aligned_sptr function for a much simpler declaration
+	*/
 	auto a = aligned_sptr<uint8_t>(64, 100);
 
 	printf("x (unique) has alignment 8: %p\n", x.get());
