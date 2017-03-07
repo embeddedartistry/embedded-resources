408905bf1fa9273c812c13a83e4d2ade95cb8c30
Phillip Johnston <phillip.johnston@gatech.edu>
2017-03-07 08:19:30 -0800

    Update compiler rules.mk.  We keep buildresults/ flat.  This will be needed for some libc work.

--- build/rules.mk
+++ build/rules.mk
@@ -1,19 +1,23 @@
+# NOTE: $(@F) gets the file portion of the target
+# This means buildresults is flat.
+# https://www.gnu.org/software/make/manual/html_node/Automatic-Variables.html
+
 # C Files
 %.o : %.c
 	$(Q)[ -d "$(BUILDRESULTS)" ] || mkdir -p $(BUILDRESULTS)
-	$(Q)$(CC) $(INCLUDES) $(CPPFLAGS) $(CFLAGS) -c -o $(BUILDRESULTS)/$@ $<
+	$(Q)$(CC) $(INCLUDES) $(CPPFLAGS) $(CFLAGS) -c -o $(BUILDRESULTS)/$(@F) $<
 
 # C++ Files
 %.o : %.cpp
 	$(Q)[ -d "$(BUILDRESULTS)" ] || mkdir -p $(BUILDRESULTS)
-	$(Q)$(CXX) $(INCLUDES) $(CPPFLAGS) $(CXXFLAGS) -c -o $(BUILDRESULTS)/$@ $<
+	$(Q)$(CXX) $(INCLUDES) $(CPPFLAGS) $(CXXFLAGS) -c -o $(BUILDRESULTS)/$(@F) $<
 
 # Preprocessed Assembly Files
 %.o : %.S
 	$(Q)[ -d "$(BUILDRESULTS)" ] || mkdir -p $(BUILDRESULTS)
-	$(Q)$(CC) $(INCLUDES) $(CPPFLAGS) $(CFLAGS) -c -o $(BUILDRESULTS)/$@ $<
+	$(Q)$(CC) $(INCLUDES) $(CPPFLAGS) $(CFLAGS) -c -o $(BUILDRESULTS)/$(@F) $<
 
 # Assembly Files
 %.o : %.s
 	$(Q)[ -d "$(BUILDRESULTS)" ] || mkdir -p $(BUILDRESULTS)
-	$(Q)$(AS) $(ASFLAGS) -c -o $(BUILDRESULTS)/$@ $<
+	$(Q)$(AS) $(ASFLAGS) -c -o $(BUILDRESULTS)/$(@F) $<
