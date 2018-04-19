0b47006dfc02db363efd99374f6f10ab71851a5d
Phillip Johnston <phillip@embeddedartistry.com>
2018-04-18 17:37:40 -0700

    Implement a second version of put which returns an error if the buffer is full
    Fixes #19

--- examples/c/circular_buffer.c
+++ examples/c/circular_buffer.c
@@ -18,7 +18,10 @@ typedef struct {
 */
 
 int circular_buf_reset(circular_buf_t * cbuf);
+/// Put Version 1 allows you to keep adding entries if the buffer is full
 int circular_buf_put(circular_buf_t * cbuf, uint8_t data);
+/// Put Version 2 returns an error if the buffer is full
+int circular_buf_put2(circular_buf_t * cbuf, uint8_t data);
 //TODO: int circular_buf_put_range(circular_buf_t cbuf, uint8_t * data, size_t len);
 int circular_buf_get(circular_buf_t * cbuf, uint8_t * data);
 //TODO: int circular_buf_get_range(circular_buf_t cbuf, uint8_t *data, size_t len);
@@ -44,6 +47,26 @@ int circular_buf_put(circular_buf_t * cbuf, uint8_t data)
     int r = -1;
 
     if(cbuf)
+    {
+        cbuf->buffer[cbuf->head] = data;
+        cbuf->head = (cbuf->head + 1) % cbuf->size;
+
+        if(cbuf->head == cbuf->tail)
+        {
+            cbuf->tail = (cbuf->tail + 1) % cbuf->size;
+        }
+
+        r = 0;
+    }
+
+    return r;
+}
+
+int circular_buf_put2(circular_buf_t * cbuf, uint8_t data)
+{
+    int r = -1;
+
+    if(cbuf && !circular_buf_full(cbuf))
     {
         cbuf->buffer[cbuf->head] = data;
         cbuf->head = (cbuf->head + 1) % cbuf->size;
@@ -122,6 +145,23 @@ int main(void)
 	for(uint8_t i = 0; i < cbuf.size + 5; i++)
 	{
 		circular_buf_put(&cbuf, i);
+	}
+
+	printf("Full: %d, empty: %d\n", circular_buf_full(&cbuf), circular_buf_empty(&cbuf));
+
+	printf("Reading back values: ");
+	while(!circular_buf_empty(&cbuf))
+	{
+		uint8_t data;
+		circular_buf_get(&cbuf, &data);
+		printf("%u ", data);
+	}
+	printf("\n");
+
+	printf("Adding 15 values using non-overwrite version\n");
+	for(uint8_t i = 0; i < cbuf.size + 5; i++)
+	{
+		circular_buf_put2(&cbuf, i);
 	}
 
 	printf("Full: %d, empty: %d\n", circular_buf_full(&cbuf), circular_buf_empty(&cbuf));
