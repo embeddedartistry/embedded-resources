d1434a70a650f1d5ec9241af83a9012631637d24
Phillip Johnston <phillip@embeddedartistry.com>
2018-08-04 16:09:37 -0700

    Update C++ circular buffer implementation
    Match the C implementation changes:
    - don't waste buffer slot
    - add capacity function
    - calculate current buffer fill level

--- examples/cpp/circular_buffer.cpp
+++ examples/cpp/circular_buffer.cpp
@@ -3,20 +3,14 @@
 #include <memory>
 #include <mutex>
 
-/**
-* Important Usage Note: This library reserves one spare entry for queue-full detection
-* Otherwise, corner cases and detecting difference between full/empty is hard.
-* You are not seeing an accidental off-by-one.
-*/
-
 template <class T>
 class circular_buffer {
 public:
 	explicit circular_buffer(size_t size) :
 		buf_(std::unique_ptr<T[]>(new T[size])),
-		size_(size)
+		max_size_(size)
 	{
-		//empty constructor
+
 	}
 
 	void put(T item)
@@ -24,15 +18,18 @@ public:
 		std::lock_guard<std::mutex> lock(mutex_);
 
 		buf_[head_] = item;
-		head_ = (head_ + 1) % size_;
 
-		if(head_ == tail_)
+		if(full_)
 		{
-			tail_ = (tail_ + 1) % size_;
+			tail_ = (tail_ + 1) % max_size_;
 		}
+
+		head_ = (head_ + 1) % max_size_;
+
+		full_ = head_ == tail_;
 	}
 
-	T get(void)
+	T get()
 	{
 		std::lock_guard<std::mutex> lock(mutex_);
 
@@ -43,32 +40,53 @@ public:
 
 		//Read data and advance the tail (we now have a free space)
 		auto val = buf_[tail_];
-		tail_ = (tail_ + 1) % size_;
+		full_ = false;
+		tail_ = (tail_ + 1) % max_size_;
 
 		return val;
 	}
 
-	void reset(void)
+	void reset()
 	{
 		std::lock_guard<std::mutex> lock(mutex_);
 		head_ = tail_;
+		full_ = false;
 	}
 
-	bool empty(void) const
+	bool empty() const
 	{
 		//if head and tail are equal, we are empty
-		return head_ == tail_;
+		return (!full_ && (head_ == tail_));
 	}
 
-	bool full(void) const
+	bool full() const
 	{
 		//If tail is ahead the head by 1, we are full
-		return ((head_ + 1) % size_) == tail_;
+		return full_;
 	}
 
-	size_t size(void) const
+	size_t capacity() const
 	{
-		return size_ - 1;
+		return max_size_;
+	}
+
+	size_t size() const
+	{
+		size_t size = max_size_;
+
+		if(!full_)
+		{
+			if(head_ >= tail_)
+			{
+				size = head_ - tail_;
+			}
+			else
+			{
+				size = max_size_ + head_ - tail_;
+			}
+		}
+
+		return size;
 	}
 
 private:
@@ -76,23 +94,37 @@ private:
 	std::unique_ptr<T[]> buf_;
 	size_t head_ = 0;
 	size_t tail_ = 0;
-	size_t size_;
+	const size_t max_size_;
+	bool full_ = 0;
 };
 
 int main(void)
 {
 	circular_buffer<uint32_t> circle(10);
 	printf("\n === CPP Circular buffer check ===\n");
-	printf("Capacity: %zu\n", circle.size());
+	printf("Size: %zu, Capacity: %zu\n", circle.size(), circle.capacity());
+
 	uint32_t x = 1;
 	printf("Put 1, val: %d\n", x);
 	circle.put(x);
+
 	x = circle.get();
 	printf("Popped: %d\n", x);
+
 	printf("Empty: %d\n", circle.empty());
 
-	printf("Adding 9 values\n");
-	for(uint32_t i = 0; i < circle.size(); i++)
+	printf("Adding %zu values\n", circle.capacity() - 1);
+	for(uint32_t i = 0; i < circle.capacity() - 1; i++)
+	{
+		circle.put(i);
+	}
+
+	circle.reset();
+
+	printf("Full: %d\n", circle.full());
+
+	printf("Adding %zu values\n", circle.capacity());
+	for(uint32_t i = 0; i < circle.capacity(); i++)
 	{
 		circle.put(i);
 	}
