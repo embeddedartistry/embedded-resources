dd3f06c1a0119b9873efa7ac9a2821f38b781328
Phillip Johnston <phillip@embeddedartistry.com>
2021-10-12 10:30:45 -0700

    Upgrade build to support testing with CMocka

---
+++ test/main.c
@@ -0,0 +1,19 @@
+/*
+ * Copyright © 2021 Embedded Artistry LLC.
+ * See LICENSE file for licensing information.
+ */
+
+// CMocka needs these
+// clang-format off
+#include <setjmp.h>
+#include <stdarg.h>
+#include <stddef.h>
+#include <cmocka.h>
+// clang-format on
+
+int main(void)
+{
+	int overall_result = 0;
+
+	return overall_result;
+}
