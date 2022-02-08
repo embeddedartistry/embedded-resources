00c1125f1bc3e1369c5022c54dd58eda40834d39
Phillip Johnston <phillip@embeddedartistry.com>
2022-02-07 17:13:54 -0800

    Change the input parameter name for the circular buffer class

--- examples/c/circular_buffer/circular_buffer_no_modulo.c
+++ examples/c/circular_buffer/circular_buffer_no_modulo.c
@@ -27,17 +27,17 @@ static inline size_t advance_headtail_value(size_t value, size_t max)
 	return value;
 }
 
-static void advance_head_pointer(cbuf_handle_t cbuf)
+static void advance_head_pointer(cbuf_handle_t me)
 {
-	assert(cbuf);
+	assert(me);
 
-	if(circular_buf_full(cbuf))
+	if(circular_buf_full(me))
 	{
-		cbuf->tail = advance_headtail_value(cbuf->tail, cbuf->max);
+		me->tail = advance_headtail_value(me->tail, me->max);
 	}
 
-	cbuf->head = advance_headtail_value(cbuf->head, cbuf->max);
-	cbuf->full = (cbuf->head == cbuf->tail);
+	me->head = advance_headtail_value(me->head, me->max);
+	me->full = (me->head == me->tail);
 }
 
 #pragma mark - APIs -
@@ -58,85 +58,85 @@ cbuf_handle_t circular_buf_init(uint8_t* buffer, size_t size)
 	return cbuf;
 }
 
-void circular_buf_free(cbuf_handle_t cbuf)
+void circular_buf_free(cbuf_handle_t me)
 {
-	assert(cbuf);
-	free(cbuf);
+	assert(me);
+	free(me);
 }
 
-void circular_buf_reset(cbuf_handle_t cbuf)
+void circular_buf_reset(cbuf_handle_t me)
 {
-	assert(cbuf);
+	assert(me);
 
-	cbuf->head = 0;
-	cbuf->tail = 0;
-	cbuf->full = false;
+	me->head = 0;
+	me->tail = 0;
+	me->full = false;
 }
 
-size_t circular_buf_size(cbuf_handle_t cbuf)
+size_t circular_buf_size(cbuf_handle_t me)
 {
-	assert(cbuf);
+	assert(me);
 
-	size_t size = cbuf->max;
+	size_t size = me->max;
 
-	if(!circular_buf_full(cbuf))
+	if(!circular_buf_full(me))
 	{
-		if(cbuf->head >= cbuf->tail)
+		if(me->head >= me->tail)
 		{
-			size = (cbuf->head - cbuf->tail);
+			size = (me->head - me->tail);
 		}
 		else
 		{
-			size = (cbuf->max + cbuf->head - cbuf->tail);
+			size = (me->max + me->head - me->tail);
 		}
 	}
 
 	return size;
 }
 
-size_t circular_buf_capacity(cbuf_handle_t cbuf)
+size_t circular_buf_capacity(cbuf_handle_t me)
 {
-	assert(cbuf);
+	assert(me);
 
-	return cbuf->max;
+	return me->max;
 }
 
-void circular_buf_put(cbuf_handle_t cbuf, uint8_t data)
+void circular_buf_put(cbuf_handle_t me, uint8_t data)
 {
-	assert(cbuf && cbuf->buffer);
+	assert(me && me->buffer);
 
-	cbuf->buffer[cbuf->head] = data;
+	me->buffer[me->head] = data;
 
-	advance_head_pointer(cbuf);
+	advance_head_pointer(me);
 }
 
-int circular_buf_try_put(cbuf_handle_t cbuf, uint8_t data)
+int circular_buf_try_put(cbuf_handle_t me, uint8_t data)
 {
 	int r = -1;
 
-	assert(cbuf && cbuf->buffer);
+	assert(me && me->buffer);
 
-	if(!circular_buf_full(cbuf))
+	if(!circular_buf_full(me))
 	{
-		cbuf->buffer[cbuf->head] = data;
-		advance_head_pointer(cbuf);
+		me->buffer[me->head] = data;
+		advance_head_pointer(me);
 		r = 0;
 	}
 
 	return r;
 }
 
-int circular_buf_get(cbuf_handle_t cbuf, uint8_t* data)
+int circular_buf_get(cbuf_handle_t me, uint8_t* data)
 {
-	assert(cbuf && data && cbuf->buffer);
+	assert(me && data && me->buffer);
 
 	int r = -1;
 
-	if(!circular_buf_empty(cbuf))
+	if(!circular_buf_empty(me))
 	{
-		*data = cbuf->buffer[cbuf->tail];
-		cbuf->tail = advance_headtail_value(cbuf->tail, cbuf->max);
-		cbuf->full = false;
+		*data = me->buffer[me->tail];
+		me->tail = advance_headtail_value(me->tail, me->max);
+		me->full = false;
 
 		r = 0;
 	}
@@ -144,38 +144,38 @@ int circular_buf_get(cbuf_handle_t cbuf, uint8_t* data)
 	return r;
 }
 
-bool circular_buf_empty(cbuf_handle_t cbuf)
+bool circular_buf_empty(cbuf_handle_t me)
 {
-	assert(cbuf);
+	assert(me);
 
-	return (!circular_buf_full(cbuf) && (cbuf->head == cbuf->tail));
+	return (!circular_buf_full(me) && (me->head == me->tail));
 }
 
-bool circular_buf_full(cbuf_handle_t cbuf)
+bool circular_buf_full(cbuf_handle_t me)
 {
-	assert(cbuf);
+	assert(me);
 
-	return cbuf->full;
+	return me->full;
 }
 
-int circular_buf_peek(cbuf_handle_t cbuf, uint8_t* data, unsigned int look_ahead_counter)
+int circular_buf_peek(cbuf_handle_t me, uint8_t* data, unsigned int look_ahead_counter)
 {
 	int r = -1;
 	size_t pos;
 
-	assert(cbuf && data && cbuf->buffer);
+	assert(me && data && me->buffer);
 
 	// We can't look beyond the current buffer size
-	if(circular_buf_empty(cbuf) || look_ahead_counter > circular_buf_size(cbuf))
+	if(circular_buf_empty(me) || look_ahead_counter > circular_buf_size(me))
 	{
 		return r;
 	}
 
-	pos = cbuf->tail;
+	pos = me->tail;
 	for(unsigned int i = 0; i < look_ahead_counter; i++)
 	{
-		data[i] = cbuf->buffer[pos];
-		pos = advance_headtail_value(pos, cbuf->max);
+		data[i] = me->buffer[pos];
+		pos = advance_headtail_value(pos, me->max);
 	}
 
 	return 0;
--- examples/c/circular_buffer/circular_buffer_no_modulo_threadsafe.c
+++ examples/c/circular_buffer/circular_buffer_no_modulo_threadsafe.c
@@ -46,130 +46,130 @@ cbuf_handle_t circular_buf_init(uint8_t* buffer, size_t size)
 	return cbuf;
 }
 
-void circular_buf_free(cbuf_handle_t cbuf)
+void circular_buf_free(cbuf_handle_t me)
 {
-	assert(cbuf);
-	free(cbuf);
+	assert(me);
+	free(me);
 }
 
-void circular_buf_reset(cbuf_handle_t cbuf)
+void circular_buf_reset(cbuf_handle_t me)
 {
-	assert(cbuf);
+	assert(me);
 
-	cbuf->head = 0;
-	cbuf->tail = 0;
+	me->head = 0;
+	me->tail = 0;
 }
 
-size_t circular_buf_size(cbuf_handle_t cbuf)
+size_t circular_buf_size(cbuf_handle_t me)
 {
-	assert(cbuf);
+	assert(me);
 
 	// We account for the space we can't use for thread safety
-	size_t size = cbuf->max - 1;
+	size_t size = me->max - 1;
 
-	if(!circular_buf_full(cbuf))
+	if(!circular_buf_full(me))
 	{
-		if(cbuf->head >= cbuf->tail)
+		if(me->head >= me->tail)
 		{
-			size = (cbuf->head - cbuf->tail);
+			size = (me->head - me->tail);
 		}
 		else
 		{
-			size = (cbuf->max + cbuf->head - cbuf->tail);
+			size = (me->max + me->head - me->tail);
 		}
 	}
 
 	return size;
 }
 
-size_t circular_buf_capacity(cbuf_handle_t cbuf)
+size_t circular_buf_capacity(cbuf_handle_t me)
 {
-	assert(cbuf);
+	assert(me);
 
 	// We account for the space we can't use for thread safety
-	return cbuf->max - 1;
+	return me->max - 1;
 }
 
 /// For thread safety, do not use put - use try_put.
 /// Because this version, which will overwrite the existing contents
 /// of the buffer, will involve modifying the tail pointer, which is also
 /// modified by get.
-void circular_buf_put(cbuf_handle_t cbuf, uint8_t data)
+void circular_buf_put(cbuf_handle_t me, uint8_t data)
 {
-	assert(cbuf && cbuf->buffer);
+	assert(me && me->buffer);
 
-	cbuf->buffer[cbuf->head] = data;
-	if(circular_buf_full(cbuf))
+	me->buffer[me->head] = data;
+	if(circular_buf_full(me))
 	{
 		// THIS CONDITION IS NOT THREAD SAFE
-		cbuf->tail = advance_headtail_value(cbuf->tail, cbuf->max);
+		me->tail = advance_headtail_value(me->tail, me->max);
 	}
 
-	cbuf->head = advance_headtail_value(cbuf->head, cbuf->max);
+	me->head = advance_headtail_value(me->head, me->max);
 }
 
-int circular_buf_try_put(cbuf_handle_t cbuf, uint8_t data)
+int circular_buf_try_put(cbuf_handle_t me, uint8_t data)
 {
-	assert(cbuf && cbuf->buffer);
+	assert(me && me->buffer);
 
 	int r = -1;
 
-	if(!circular_buf_full(cbuf))
+	if(!circular_buf_full(me))
 	{
-		cbuf->buffer[cbuf->head] = data;
-		cbuf->head = advance_headtail_value(cbuf->head, cbuf->max);
+		me->buffer[me->head] = data;
+		me->head = advance_headtail_value(me->head, me->max);
 		r = 0;
 	}
 
 	return r;
 }
 
-int circular_buf_get(cbuf_handle_t cbuf, uint8_t* data)
+int circular_buf_get(cbuf_handle_t me, uint8_t* data)
 {
-	assert(cbuf && data && cbuf->buffer);
+	assert(me && data && me->buffer);
 
 	int r = -1;
 
-	if(!circular_buf_empty(cbuf))
+	if(!circular_buf_empty(me))
 	{
-		*data = cbuf->buffer[cbuf->tail];
-		cbuf->tail = advance_headtail_value(cbuf->tail, cbuf->max);
+		*data = me->buffer[me->tail];
+		me->tail = advance_headtail_value(me->tail, me->max);
 		r = 0;
 	}
 
 	return r;
 }
 
-bool circular_buf_empty(cbuf_handle_t cbuf)
+bool circular_buf_empty(cbuf_handle_t me)
 {
-	assert(cbuf);
-	return cbuf->head == cbuf->tail;
+	assert(me);
+	return me->head == me->tail;
 }
 
-bool circular_buf_full(circular_buf_t* cbuf)
+bool circular_buf_full(cbuf_handle_t me)
 {
 	// We want to check, not advance, so we don't save the output here
-	return advance_headtail_value(cbuf->head, cbuf->max) == cbuf->tail;
+	return advance_headtail_value(me->head, me->max) == me->tail;
 }
 
-int circular_buf_peek(cbuf_handle_t cbuf, uint8_t* data, unsigned int look_ahead_counter)
+int circular_buf_peek(cbuf_handle_t me, uint8_t* data, unsigned int look_ahead_counter)
 {
 	int r = -1;
 	size_t pos;
 
-	assert(cbuf && data && cbuf->buffer);
+	assert(me && data && me->buffer);
 
 	// We can't look beyond the current buffer size
-	if(circular_buf_empty(cbuf) || look_ahead_counter > circular_buf_size(cbuf))
+	if(circular_buf_empty(me) || look_ahead_counter > circular_buf_size(me))
 	{
 		return r;
 	}
 
-	pos = cbuf->tail;
+	pos = me->tail;
 	for(unsigned int i = 0; i < look_ahead_counter; i++)
 	{
-		data[i] = cbuf->buffer[pos];
-		pos = advance_headtail_value(pos, cbuf->max);
+		data[i] = me->buffer[pos];
+		pos = advance_headtail_value(pos, me->max);
 	}
 
 	return 0;
